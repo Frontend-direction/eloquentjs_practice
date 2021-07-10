32f315949ea8dbcd5d0d3fedc1c57f778041be45
vovapankiw <vovapankiw@gmail.com>
2021-07-10 11:25:54 +0300

    task create collection

---
+++ 20.Nodejs/createDir.js
@@ -0,0 +1,18 @@
+const { mkdir, stat } = require("fs").promises;
+
+methods.MKCOL = async function(request) {
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch (error) {
+    if (error.code != "ENOENT") throw error;
+    await mkdir(path);
+    return {status: 204};
+  }
+  if (stats.isDirectory()) {
+    return {status: 204};
+  } else {
+    return { status: 400, body: "Not a directory" };
+  }
+};
